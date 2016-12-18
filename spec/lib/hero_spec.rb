74756feec76974d4e4d88fe554904ad5365b01b4
Marius Maries <mariuscm@gmail.com>
2016-12-18 03:49:29 -0600

    Skill_check returns the result of a check

--- spec/lib/hero_spec.rb
+++ spec/lib/hero_spec.rb
@@ -25,14 +25,14 @@ describe Hero do
 
 	describe "attack attack" do
 		it "succeeds" do
-			dicepool.stub(:skill_check).and_return(3)
+			dicepool.stub(:skill_check).and_return(true)
 			hero = Hero.new dicepool: dicepool
 			monster = double("monster", toughness: 2)
 			expect(hero.attack(monster)).to be true
 		end
 
 		it "fails" do
-			dicepool.stub(:skill_check).and_return(1)
+			dicepool.stub(:skill_check).and_return(false)
 			hero = Hero.new dicepool: dicepool
 			monster = double("monster", toughness: 2)
 			expect(hero.attack(monster)).to be false
