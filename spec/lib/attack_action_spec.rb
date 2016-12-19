2f1c22e7e6cb45e831c5159dfa27b2b07ce99ecf
Marius Maries <mariuscm@gmail.com>
2016-12-18 21:46:53 -0600

    AttackAction effect

--- spec/lib/attack_action_spec.rb
+++ spec/lib/attack_action_spec.rb
@@ -2,23 +2,52 @@ require 'spec_helper'
 require 'attack_action'
 
 describe AttackAction do
-	let(:hero) { double("hero", strength: 3)}
+	let(:hero) { double("hero", strength: 3, gain_exp: nil, gain_gold: nil, damage:nil) }
 	let(:dicepool) { double("dicepool") }
-	let(:action) { AttackAction.new hero, dicepool}
-	
-	
-	it "makes strength check against target toughness" do
-		monster = double("monster", toughness: 2)
-		dicepool.should_receive(:skill_check).with(hero.strength, monster.toughness)
-		action.activate(monster)
-	end
+	let(:action) { AttackAction.new hero, dicepool }
+	let(:monster) { double("monster", toughness: 2, kill: nil, damage:4) }
 
-	it "responds to activate message" do
-		expect(action).to respond_to(:activate)
-	end
+	describe "effect" do
+		context "success" do
+			before :each do
+				allow(dicepool).to receive(:skill_check).and_return(true)
+			end
 
-	it "has an owner" do
-		expect(action.owner).to eq(hero)
+			it "kills monster" do
+				expect(monster).to receive(:kill)
+				action.activate(monster)
+			end
+
+			it "rewards owner with exp" do
+				expect(hero).to receive(:gain_exp)
+				action.activate(monster)
+			end
+			it "rewards owner with gold" do
+				expect(hero).to receive(:gain_gold)
+				action.activate(monster)
+			end
+
+			context "failure" do
+				it "damages owner" do
+					allow(dicepool).to receive(:skill_check).and_return(false)
+					expect(hero).to receive(:damage).with(monster.damage)
+					action.activate(monster)
+				end
+			end
+		end
+
+		it "makes strength check against target toughness" do			
+			expect(dicepool).to receive(:skill_check).with(hero.strength, monster.toughness)
+			action.activate(monster)
+		end
+
+		it "responds to activate message" do
+			expect(action).to respond_to(:activate)
+		end
+
+		it "has an owner" do
+			expect(action.owner).to eq(hero)
+		end
 	end
 
 end
