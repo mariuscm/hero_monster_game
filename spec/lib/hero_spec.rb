bb05410762037c0ecb40e63ed0bd8790608c5392
Marius Maries <mariuscm@gmail.com>
2016-12-18 23:22:59 -0600

    Add needed attributes and methods to Hero class for AttackAction to work

--- spec/lib/hero_spec.rb
+++ spec/lib/hero_spec.rb
@@ -3,15 +3,39 @@ require 'hero'
 
 describe Hero do
 	let(:dicepool) {double("dicepool")}
+	let(:hero) { Hero.new }
 
 	describe "default attributes" do
-		let(:hero) { Hero.new }
 		it "has default strength equal to 3" do
 			expect(hero.strength).to eq(3)
 		end
 		it "has default health equal to 10" do
 			expect(hero.health).to eq(10)
 		end
+		it "has default gold equal to 0" do
+			expect(hero.gold).to eq(0)
+		end
+		it "has default experience equal to 0" do
+			expect(hero.experience).to eq(0)
+		end
+	end
+
+	it "can be damaged" do
+		hero.damage(3)
+		hero.damage(2)
+		expect(hero.health).to eq(5)
+	end
+
+	it "gains gold" do
+		hero.gain_gold(4)
+		hero.gain_gold(7)
+		expect(hero.gold).to eq(11)
+	end
+
+	it "gains experience" do
+		hero.gain_experience(4)
+		hero.gain_experience(7)
+		expect(hero.experience).to eq(11)
 	end
 
 	it "can be initialized with custom strength" do
